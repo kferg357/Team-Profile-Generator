03978d88103e4b4b711b8f673d876a641eaeaba3
kferg357 <kennethferguson90@gmail.com>
2021-06-09 10:00:25 -0400

    added html

--- index.js
+++ index.js
@@ -134,6 +134,26 @@ function addNewMember() {
         })
 }
 
+function gerenateInitailHTML() {
+    return `<!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team Profile</title>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    
+    <body>
+        <div class="teamNavBar">
+            <h class="navBarTitle">My team</h>
+        </div>
+        <div class="cardBody">`
+}
+
+
 
 function generateHTML(){
     fs.writeFileSync(generatedHtmlFilePath,"");
