f7a6b4cb75cdea59cf8d609643c866990a7f87ce
kferg357 <kennethferguson90@gmail.com>
2021-08-31 09:16:57 -0400

    added info

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -4,6 +4,7 @@ const Manager = require("../lib/Manager");
 test("Ability to set office number using constructor function", () => {
     const officeNumber = "1738";
     const employee = new Manager("Ferguson", 1, "ferguson@gmail.com", officeNumber);
+    
     expect(employee.officeNumber).toBe(officeNumber);
   });
 
